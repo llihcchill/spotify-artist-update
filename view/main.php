acfca62afa6edc54d3f322d54aa99b0bb2de96e1
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-06-21 21:10:38 +1000

    Update main.php

--- view/main.php
+++ view/main.php
@@ -1,3 +1,15 @@
+<html>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+</html>
+
+<body>
+  <!-- navbar -->
+
+  <!-- form to get all the playlist information-->
+</body>
+
+
 <?php
-echo "worky";
+  echo "main";
 ?>
