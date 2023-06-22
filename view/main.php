d299bc522cb8e7abbc1045d16f1fe41d698d3277
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-06-22 20:17:54 +1000

    Update main.php

--- view/main.php
+++ view/main.php
@@ -1,12 +1,20 @@
 <html>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+  <meta charset="utf-8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+  <link rel="stylesheet" href="/bootstrap-theme/theme.css"/>
+  <!-- google fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet"/>
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"/>
 </html>
 
 <body>
   <!-- navbar -->
 
   <!-- form to get all the playlist information-->
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="/bootstrap-theme/theme.js"></script>
 </body>
 
 
