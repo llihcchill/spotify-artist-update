2a85b0bf7ddea3df030802e21528e93d38588100
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-06-22 20:52:45 +1000

    Update main.php

--- view/main.php
+++ view/main.php
@@ -2,21 +2,34 @@
   <meta charset="utf-8"/>
   <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-  <link rel="stylesheet" href="/bootstrap-theme/theme.css"/>
+  <link rel="stylesheet" href="./view/bootstrap-theme/theme.css"/>
   <!-- google fonts -->
   <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet"/>
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"/>
 </html>
 
 <body>
-  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Artist Update</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
   <!-- form to get all the playlist information-->
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="/bootstrap-theme/theme.js"></script>
+  <script src="./view/bootstrap-theme/theme.js"></script>
 </body>
-
 
 <?php
   echo "main";
