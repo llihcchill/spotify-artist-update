a6f61cd2c73016b68a46ca76c026c755c2c7d4a8
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-06-26 00:00:00 +1000

    Update callback.php

--- callback.php
+++ callback.php
@@ -1,39 +1,39 @@
 <?php
-  use Symfony\Component\HttpClient\HttpClient;
-
   $code = $_GET["code"];
   $state = $_GET["state"];
   $client_secret = "CLIENT_SECRET";
+  $url = "https://accounts.spotify.com/api/token";
 
   require_once("./controller/login.php");
 
   if($code === null) {
-    return require_once("./view/main.php?");
+    return require_once("./view/main.php");
   } else {
-    $form = array(
+    $data = array(
       "code" => $code,
       "redirect_uri" => "http://localhost:8080/callback.php",
-      "grant_type" => "authorization_code"
+      "grant_type" => "authorization_code",
+      "json" => true
     );
 
     $headers = array(
       "Authorization" => "Basic " . base64_encode($client_id . ":" . $client_secret)
     );
 
-    $json = array(
-      "json" => true
+    $options = array(
+      "http" => array(
+        "header" => "Authorization: Basic " . base64_encode($client_id . ":" . $client_secret),
+        "method" => "POST",
+        "content" => http_build_query($data)
+      )
     );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    
+    if ($result === FALSE) {
+      echo "not good";
+    }
 
-    $form_json = json_encode($form);
-    $headers_json = json_encode($headers);
-    $json_json = json_encode($json);
-
-    $authorised = "https://accounts.spotify.com/api/token" .  urlencode($form_json) . urlencode($headers_json) . urlencode($json_json);
-
-    $client = HttpClient::create();
-    $request = $client->request(
-      "POST",
-      $authorised
-    );
+    var_dump($result);
   }
 ?>
