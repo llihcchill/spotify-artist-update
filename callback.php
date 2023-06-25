25a41936c0f4d60ba54268a05cb5e154520ebf3a
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-06-26 07:53:37 +1000

    Update callback.php

--- callback.php
+++ callback.php
@@ -1,21 +1,5 @@
 <?php
-  function http_request(array $data, string $header, string $get_or_post_request, string $url) {
-    $options = array(
-      "http" => array(
-        "header" => $header,
-        "method" => $get_or_post_request,
-        "content" => http_build_query($data)
-      )
-    );
-    
-    $context = stream_context_create($options);
-    global $result;
-    $result = file_get_contents($url, false, $context);
-
-    if ($result === FALSE) {
-      return error_log("HTTP/1.1 request has failed, as it could not get the results of the request.");
-    }
-  }
+  require_once("./functions.php");
 
   $code = $_GET["code"];
   $state = $_GET["state"];
@@ -34,16 +18,13 @@
       "json" => true
     );
 
-    http_request(
-      $data,
+    http_request($data,
       "Authorization: Basic " . base64_encode($client_id . ":" . $client_secret),
-      "POST",
-      $url
+      "POST", $url
     );
     $decode = json_decode($result);
 
-    echo $result;
-    echo "<br>";
-    echo "this is your access token: " . $decode->access_token;
+    $access_token = $decode->access_token;
+    $refresh_token = $decode->refresh_token;
   }
 ?>
