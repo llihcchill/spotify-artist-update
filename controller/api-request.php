04e60b25fc6cbdeb828ead99c405d49c0127249f
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-06-25 20:46:56 +1000

    Update api-request.php

--- controller/api-request.php
+++ controller/api-request.php
@@ -1,3 +1,13 @@
 <?php
-  echo "api request";
+  use Symfony\Component\HttpClient\HttpClient;
+
+  $url_query = "example";
+
+  // create an HTTP client to send HTTP requests
+  $client = HttpClient::create();
+  $request = $client->request(
+    "GET",
+    $url_query,
+    // add header
+  );
 ?>
