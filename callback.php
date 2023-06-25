ea5abaea0196c6d34b144ed690ccbe39df5e03ee
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-06-26 07:24:22 +1000

    Update callback.php

--- callback.php
+++ callback.php
@@ -1,4 +1,22 @@
 <?php
+  function http_request(array $data, string $header, string $get_or_post_request, string $url) {
+    $options = array(
+      "http" => array(
+        "header" => $header,
+        "method" => $get_or_post_request,
+        "content" => http_build_query($data)
+      )
+    );
+    
+    $context = stream_context_create($options);
+    global $result;
+    $result = file_get_contents($url, false, $context);
+
+    if ($result === FALSE) {
+      return error_log("HTTP/1.1 request has failed, as it could not get the results of the request.");
+    }
+  }
+
   $code = $_GET["code"];
   $state = $_GET["state"];
   $client_secret = "CLIENT_SECRET";
@@ -16,20 +34,16 @@
       "json" => true
     );
 
-    $options = array(
-      "http" => array(
-        "header" => "Authorization: Basic " . base64_encode($client_id . ":" . $client_secret),
-        "method" => "POST",
-        "content" => http_build_query($data)
-      )
+    http_request(
+      $data,
+      "Authorization: Basic " . base64_encode($client_id . ":" . $client_secret),
+      "POST",
+      $url
     );
-    $context = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    
-    if ($result === FALSE) {
-      echo "not good";
-    }
+    $decode = json_decode($result);
 
-    var_dump($result);
+    echo $result;
+    echo "<br>";
+    echo "this is your access token: " . $decode->access_token;
   }
 ?>
