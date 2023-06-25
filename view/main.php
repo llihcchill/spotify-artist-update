d23dffb7050cf5e5756e51947774cb2751230e64
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-06-25 14:58:02 +1000

    Update main.php

--- view/main.php
+++ view/main.php
@@ -11,14 +11,14 @@
 <body>
   <nav class="navbar navbar-expand-lg navbar-light fixed-top">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Artist Update</a>
+      <a class="navbar-brand text-light" href="#">Artist Update</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
           </li>
         </ul>
       </div>
@@ -28,11 +28,17 @@
     <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
       <div class="d-flex justify-content-center">
         <div class="text-center">
-          <h1 class="mx-auto my-0 text-uppercase">Form head</h1>
-          <h2 class="text-white-50 mx-auto mt-2 mb-5">form description</h2>
-
-          <!-- form to get all the playlist information-->
-
+          <h1 class="mx-auto my-0 text-uppercase">Get all the new songs from your favourate artists</h1>
+          <h2 class="text-white-50 mx-auto mt-2 mb-5">
+            Before you log in to spotify, first create a playlist containing a song from every artist
+            you want to get up to date with.
+          </h2>
+            <button class="rounded-2 text-white mx-auto mt-2 mb-5 btn btn-success" type="button">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-spotify" viewBox="0 0 16 16">
+                <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.669 11.538a.498.498 0 0 1-.686.165c-1.879-1.147-4.243-1.407-7.028-.77a.499.499 0 0 1-.222-.973c3.048-.696 5.662-.397 7.77.892a.5.5 0 0 1 .166.686zm.979-2.178a.624.624 0 0 1-.858.205c-2.15-1.321-5.428-1.704-7.972-.932a.625.625 0 0 1-.362-1.194c2.905-.881 6.517-.454 8.986 1.063a.624.624 0 0 1 .206.858zm.084-2.268C10.154 5.56 5.9 5.419 3.438 6.166a.748.748 0 1 1-.434-1.432c2.825-.857 7.523-.692 10.492 1.07a.747.747 0 1 1-.764 1.288z"/>
+              </svg>
+              Login with Spotify
+            </button>
         </div>
       </div>
     </div>
