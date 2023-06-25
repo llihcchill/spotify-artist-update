031eda5d18463ccdc01514328df94452334b5663
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-06-26 08:52:13 +1000

    Update user-playlists.php

--- controller/user-playlists.php
+++ controller/user-playlists.php
@@ -3,4 +3,7 @@
   require_once("./callback.php");
 
   $header = "Authorization: Bearer " . $access_token;
+  $data = array();
+
+  $req = http_request($data, $header, "GET", "https://api.spotify.com/v1/me");
 ?>
