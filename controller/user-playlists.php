46d910e5459db8a9176b2a3746adf83b29c9dd5d
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-06-28 07:55:31 +1000

    Update user-playlists.php

--- controller/user-playlists.php
+++ controller/user-playlists.php
@@ -4,6 +4,20 @@
 
   $header = "Authorization: Bearer " . $access_token;
   $data = array();
+  $playlist_req_data = array(
+    "limit" => 50,
+  );
 
-  $req = http_request($data, $header, "GET", "https://api.spotify.com/v1/me");
+  // make an HTTP request to the Spotify API to get the current user's Spotify ID
+  $user_req = http_request($data, $header, "GET", "https://api.spotify.com/v1/me");
+  $decode = json_decode($user_req);
+
+  $spotify_id = $decode->id;
+
+  // make an HTTP request to the Spotify API to get all of the current user's playlists
+  $playlist_req = http_request($data, $header, "GET", "https://api.spotify.com/v1/users/" . $spotify_id . "/playlists");
+  $playlist_decode = json_decode($playlist_req);
+
+  $item = $playlist_decode->item;
+  $owner_id = $i->owner->id;
 ?>
