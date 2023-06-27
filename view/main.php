25d773a11d99d2d98f2731eae12b3b9e18373907
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-06-28 08:03:03 +1000

    Update main.php

--- view/main.php
+++ view/main.php
@@ -1,11 +1,13 @@
 <html>
-  <meta charset="utf-8"/>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-  <link rel="stylesheet" href="./view/bootstrap-theme/theme.css"/>
-  <!-- google fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet"/>
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"/>
+  <head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <link rel="stylesheet" href="./view/bootstrap-theme/theme.css"/>
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"/>
+  </head>
 </html>
 
 <body>
