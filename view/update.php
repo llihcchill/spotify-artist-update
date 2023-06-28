35fa63caeffc4741c04d2ed92754af2a5baa3802
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-06-28 16:59:53 +1000

    Update update.php

--- view/update.php
+++ view/update.php
@@ -3,7 +3,9 @@
     <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <link rel="stylesheet" href="./view/bootstrap-theme/theme.css"/>
+    <!-- using normal bootstrap now-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <!-- google fonts -->
     <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet"/>
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"/>
@@ -28,7 +30,7 @@
   </nav>
 
   <?php
-    require_once("./controller/user-playlists.php");
+    require_once("../controller/user-playlists.php");
 
     foreach ($item as $i) {
         if ($owner_id !== $spotify_id) {
