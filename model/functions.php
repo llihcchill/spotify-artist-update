81243419133743b33c15f4a270601f5ceb97730d
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-07-02 16:08:25 +1000

    Update functions.php

--- model/functions.php
+++ model/functions.php
@@ -16,4 +16,19 @@
       return $result;
     }
   }
+
+  function playlist_filter($item) {
+    if ($item->owner->id !== $item->id) {
+      return;
+    } else {
+?>
+      <div class="card" style="width: 300px">
+        <img class="card-img-top" src="<?php echo $item->images[0]->url; ?>">
+        <div class="card-body">
+          <h5 class="card-title text-center"><?php echo $item->name; ?></h5>
+        </div>
+      </div>
+<?php
+    }
+  }
 ?>
