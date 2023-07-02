1a5c4816e41e47c39237b90dcc056ca00c28b9ca
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-07-02 10:32:08 +1000

    Update user-playlists.php

--- controller/user-playlists.php
+++ controller/user-playlists.php
@@ -6,22 +6,15 @@
   $header = "Authorization: Bearer " . $_SESSION["access_token"];
   $data = array();
 
-  // make an HTTP request to the Spotify API to get the current user's Spotify ID
+  // make an HTTP request to the Spotify API to get the current user to log in
   $user_req = http_request($data, $header, "GET", "https://api.spotify.com/v1/me");
-  $decode = json_decode($user_req);
-
-  $_SESSION["spotify_id"] = $decode->id;
 
   // make an HTTP request to the Spotify API to get all of the current user's playlists
-  $playlist_req = http_request($data, $header, "GET", "https://api.spotify.com/v1/users/" . $spotify_id . "/playlists");
-  $playlist_decode = json_decode($playlist_req);
+  $user_playlist_http_request = http_request($data, $header, "GET", "https://api.spotify.com/v1/me/playlists?offset=119");
+  $user_playlist_http_request_json = json_decode($user_playlist_http_request);
 
-  // set all the JSON data needed for the playlist and store them in the browser session
-  $_SESSION["item"] = $playlist_decode->items;
-  $_SESSION["owner_id"] = $playlist_decode->items[0]->owner->id;
-  $_SESSION["item_id"] = $playlist_decode->items[0]->id;
-  $_SESSION["playlist_name"] = $playlist_decode->items[0]->name;
-  $_SESSION["image"] = $playlist_decode->items[0]->images[0]->url;
+  // set the item JSON data needed for the playlists and store them in the browser session
+  $_SESSION["items"] = $playlist_decode->items;
 
   header("Location: http://localhost:8080/view/update.php");
   exit();
