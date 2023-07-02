87132658bda6849d310dc93523009c34272fb852
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-07-02 16:37:41 +1000

    Update user-playlists.php

--- controller/user-playlists.php
+++ controller/user-playlists.php
@@ -4,13 +4,14 @@
 
   session_start();
   $header = "Authorization: Bearer " . $_SESSION["access_token"];
-  $data = array();
 
   // make an HTTP request to the Spotify API to get the current user to log in
-  $user_req = http_request($data, $header, "GET", "https://api.spotify.com/v1/me");
+  $user_req = http_request(array(), $header, "GET", "https://api.spotify.com/v1/me");
+  $user_id_json = json_decode($user_req);
+  $_SESSION["user_id"] = $user_id_json->id;
 
   // make an HTTP request to the Spotify API to get all of the current user's playlists
-  $user_playlist_http_request = http_request($data, $header, "GET", "https://api.spotify.com/v1/me/playlists");
+  $user_playlist_http_request = http_request(array(), $header, "GET", "https://api.spotify.com/v1/me/playlists");
   $user_playlist_http_request_json = json_decode($user_playlist_http_request);
 
   // set the item JSON data needed for the playlists and store them in the browser session
